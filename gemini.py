883e3e02b827ed2096110113647018875eb9e51e
PlutoniaX <raywjtang@hotmail.com>
2024-11-16 14:30:26 +1100

    flash-latest

--- gemini.py
+++ gemini.py
@@ -15,7 +15,7 @@ genai.configure(api_key=os.getenv('GOOGLE_GEMINI_API_KEY'))
 
 def get_model(system_instruction):
     return genai.GenerativeModel(
-        model_name='gemini-1.5-flash',
+        model_name='gemini-1.5-flash-latest',
         system_instruction=system_instruction,
         generation_config=genai.types.GenerationConfig(
             temperature=0
